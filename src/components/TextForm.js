8d55d6b11381e97673f1bcc15cf69416a78e2571
Mayank Garg <113042462+Mayank-Garg7@users.noreply.github.com>
2024-12-06 12:02:10 +0530

    Add files via upload

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -4,32 +4,60 @@ function TextForm(props) {
     const handleChange = (event) => {
         setText(event.target.value);
         console.log("on change")
-     }
-     const changeUP = ()=>{
+    }
+    const changeUP = () => {
         let newText = text.toUpperCase();
         setText(newText);
-     }
-     const changeLP = () =>{
+    }
+    const changeLP = () => {
         let newText = text.toLowerCase();
         setText(newText);
-     }
-     const clear =()=>{
+    }
+    const clear = () => {
         setText("");
-     }
-    const[text, setText] = useState("kuch to likho re");
+    }
+    const handleCopy = () => {
+        let text = document.getElementById("myBox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+
+    }
+    const handleSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+    const handlePaste = async () => {
+
+        try {
+            const clipboardText = await navigator.clipboard.readText();
+            setText(clipboardText);
+        }
+        catch (err) {
+            console.error("Failed to read clipboard contents: ", err);
+        }
+    }
+    const [text, setText] = useState("");
 
     return (
         <>
             <div className='container my-3'>
                 <h2>{props.title}</h2>
-                <textarea name="nothing" id="ok " cols="5" rows="6" value={text} onChange={handleChange} className='form-control' />
+                <textarea name="nothing" id="myBox" cols="5" rows="6" value={text} onChange={handleChange} className='form-control' />
             </div>
-            <button className="btn btn-primary mx-2" onClick={changeUP}>toUppercase</button>
-            <button className="btn btn-primary mx-2" onClick={changeLP}>toLowecase</button>
-            <button className="btn btn-primary mx-2" onClick={clear}>Clear</button>
+            <div className="container">
+                <button className="btn btn-primary mx-2" onClick={changeUP}>upperCase</button>
+                <button className="btn btn-primary mx-2" onClick={changeLP}>loweCase</button>
+                <button className="btn btn-primary mx-2" onClick={clear}>clearText</button>
+                <button className="btn btn-primary mx-2" onClick={handleCopy}>copyText</button>
+                <button className="btn btn-primary mx-2" onClick={handleSpaces}>removeExtraSpaces</button>
+                <button className="btn btn-primary mx-2" onClick={handlePaste}>pasteText</button>
+            </div>
             <div className="container my-3 ">
                 <h2>Your text summary</h2>
                 <p> {text.split(" ").length} : words   &nbsp;&nbsp;&nbsp; {text.length} : characters </p>
+                <p>{0.02 * text.split(" ").length} minutes read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
             </div>
         </>
     )
